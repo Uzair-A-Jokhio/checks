0f9deb6956d8d628e38672a44faf4e91094bc815
Uzair <uzairarifjokhio@gmail.com>
2023-06-03 19:01:25 +0500

    added new print statement removed some of the strings

--- pythonhello.py
+++ pythonhello.py
@@ -1,6 +1,7 @@
 print("hello world")
-print("hello i am back after so longgg")
+print("hello i am back ")
 def hello_fun(name):
     print(f"hello, there {name}")
+    print("this is so cool")
 
 hello_fun("shamir")
