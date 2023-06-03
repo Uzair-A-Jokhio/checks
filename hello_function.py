2fe20e5a35edd7d9c3c23abe0fb73bb90b64846a
Uzair <uzairarifjokhio@gmail.com>
2023-06-03 19:35:37 +0500

    modify the hello function

--- hello_function.py
+++ hello_function.py
@@ -1,7 +1,7 @@
 print("hello world")
 print("hello i am back ")
 def hello_fun(name):
-    print(f"hello, there {name}")
-    print("this is so cool")
-
+    print(f"hello, there {name}\n"
+          "how are you ")
+    
 hello_fun("shamir")
