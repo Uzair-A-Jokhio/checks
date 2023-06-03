aeb9ebcd3f8a3297f4a146edd77347c9e9c6c8fb
Uzair <uzairarifjokhio@gmail.com>
2023-06-03 21:51:29 +0500

    added a all_check function..

---
+++ all_checks.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+def check_reboot():
+    ''' Retrun True if a computer has a pending reboot '''
+    return os.path.exists("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Prending Reboot..")
+        sys.exit(1)
+    if desk_full():
+        print("Disk full..")
+        sys.exit(1)
+    print("Everything okay..")
+    sys.exit(0)
+
+main()
