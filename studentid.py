9277dfb634aae20be9544a030e61efa1ecb790fd
Uzair <uzairarifjokhio@gmail.com>
2023-06-03 20:23:11 +0500

    Update 0.1

--- studentid.py
+++ studentid.py
@@ -1,8 +1,11 @@
 # this is a child branch 
+q = 5
 x = 0
 student_name = []
-while x <= 4:
-    stu = input("enter student name:")
+while x <= q:
+    stu = input(
+        f"Enter Student name \n "
+         f"Name:  ")
     student_name.append(stu)
     x += 1
 print("********************* STUDENT ID *********************")
